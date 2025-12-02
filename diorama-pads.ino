cc44cff933a001a3cfa2be60e6019dfbbd8c6285
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-12-02 13:50:37 -0700

    refactor: remove all setup and loop logic, now handled by App class

--- diorama-pads.ino
+++ diorama-pads.ino
@@ -1,97 +1,27 @@
 #include <Wire.h>
-#include "src/MyMPR121.h"
-#include "src/AudioPlayer.h"
-// #include "src/RingWindow.h"
-
-#ifndef _BV
-#define _BV(bit) (1 << (bit))
-#endif
+#include "src/App.h"
 
 #define RXI_PIN 20  // ESP32C3 RXI -> DY-HV20T TX
 #define TXO_PIN 21  // ESP32C3 TXO -> DY-HV20T RX
-#define BUSY_PIN A4
 
-const uint8_t NUM_ELECTRODES = 3;
-const uint8_t MPR121_I2C_ADDR = 0x5A;
-
-uint16_t lastTouched = 0;
-uint16_t currTouched = 0;
-
-enum class SystemState {
-  IDLE,
-  PLAYING
-};
-
-MyMPR121 mpr121 = MyMPR121();
-AudioPlayer player;
-
-SystemState currentState = SystemState::IDLE;
+App app;
 
 void setup() {
-  Wire.begin();
-  delay(100);
   Serial.begin(9600);
   delay(100);
-  Serial1.begin(9600, SERIAL_8N1, RXI_PIN, TXO_PIN);
-  // while (!Serial);
+  Wire.begin();
   delay(100);
-  pinMode(BUSY_PIN, INPUT);
+  Serial1.begin(9600, SERIAL_8N1, RXI_PIN, TXO_PIN);
   delay(3000);
 
-  if (!mpr121.begin(MPR121_I2C_ADDR, &Wire)) {
-    Serial.print("MPR121 not found, check wiring");
-    // hold indefinitely
+  if (!app.setup()) {
+    // failed, notify then hold indefinitely for now
+    Serial.println("SETUP FAILED");
+    delay(1000);
     while (1);
   }
-
-  Serial.println("Initial CDC and CDT Values:");
-  mpr121.dumpCDCandCDTRegisters();
-  delay(1000);
-
-  mpr121.setAutoconfig(true);
-
-  Serial.println("New CDC and CDT Values:");
-  mpr121.dumpCDCandCDTRegisters();
-  delay(1000);
-
-  Serial.println("=== ELECTRODE READINGS ===");
-  Serial.println("Electrode, Filtered, Baseline, Delta");
 }
 
 void loop() {
-  // read busy pin
-  bool isPlaying = (digitalRead(BUSY_PIN) == LOW);
-
-  // touched method returns a 16-bit value where each bit represents one pad
-  // so if pads 0 and 2 are currently touched:
-  // currTouched = 0b0000_0000_0000_0101
-  currTouched = mpr121.touched();
-
-  switch (currentState) {
-    case SystemState::IDLE:
-      for (uint8_t i = 0; i < NUM_ELECTRODES; i++) {
-        // it if *is* touched and *wasnt* touched before, begin playing and immediately transition state!
-        if ((currTouched & _BV(i)) && !(lastTouched & _BV(i))) {
-          Serial.print(i);
-          Serial.println(" touched");
-          player.playTrackByIndex((uint16_t)i + 1);
-          currentState = SystemState::PLAYING;
-          break;
-        }
-        mpr121.dumpCapData(i);
-      }
-      break;
-    case SystemState::PLAYING:
-      if (!isPlaying) {
-        // only transition back to IDLE state when sound is done playing
-        currentState = SystemState::IDLE;
-      } else {
-        Serial.println("PLAYING");
-      }
-      break;
-  }
-  lastTouched = currTouched;
-
-  // put a delay so it isn't overwhelming
-  delay(100);
+  app.loopOnce();
 }
