4dc893d4c2ee2e238a93288ab2bf249f07926baf
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-12-04 15:58:16 -0700

    updated state machine to 1. Create timestamp for when sound command was sent 2. Wait for a brief period in PLAYING state to ensure busy pin is asserted (pulled low) 3. Include a safety mechanism to force state transition if playback time exceeds specified amount 4. Create timestamp when sound finishes playing (busy pin pulled back to high) at which point another timestamp is created to represent the moment sound finished playing 5. Enter a new COOLDOWN state to only update stale MPR121 delta readings 6. Revert back to IDLE after cool down period with fresh delta readings and ready to detect a new touch

--- src/App.cpp
+++ src/App.cpp
@@ -42,8 +42,8 @@ void App::loopOnce() {
   case Config::AppState::DEBUG:
     runDebug();
     break;
-  case Config::AppState::RUN_PROD:
-    runProd();
+  case Config::AppState::RUN:
+    run();
     break;
   case Config::AppState::ERROR_RECOVERY:
     // TODO: implementrecover() method
@@ -61,39 +61,62 @@ void App::runDebug() {
   delay(10);
 }
 
-void App::runProd() {
-  // read busy pin
+// NOTE: query touched() only when system is IDLE! (not playing sounds)
+// or during COOLDOWN to avoid speaker interference messing with baseline
+// tracking and delta updates
+void App::run() {
   bool isPlaying = (digitalRead(Config::Audio::AUDIO_BUSY) == LOW);
 
-  // touched method returns a 16-bit value where each bit represents one pad
-  // so if pads 0 and 2 are currently touched:
-  // currTouched = 0b0000_0000_0000_0101
-  currTouched = mpr121.touched();
-
-  switch (currentProdRunState) {
-  case ProductionRunState::IDLE:
+  switch (currentRunState) {
+  case RunState::IDLE:
+    currTouched = mpr121.touched();
     for (uint8_t i = 0; i < Config::Touch::NUM_ELECTRODES; i++) {
-      // it if *is* touched and *wasnt* touched before, begin playing and
-      // immediately transition state!
       if ((currTouched & _BV(i)) && !(lastTouched & _BV(i))) {
-        Serial.print(i);
-        Serial.println(" touched");
+        // if it *is* touched and *wasnt* touched before, send UART command to
+        // DY-HV20T to play track, wait for busy pin to assert, then
+        // transition state
         player.playTrackByIndex((uint16_t)i + 1);
-        currentProdRunState = ProductionRunState::PLAYING;
+
+        playbackBeganAt = millis();
+        currentRunState = RunState::PLAYING;
         break;
       }
     }
+    lastTouched = currTouched;
     break;
-  case ProductionRunState::PLAYING:
+  case RunState::PLAYING:
+    // wait for busy pin to assert
+    if (millis() - playbackBeganAt < Config::Audio::AUDIO_BEGIN_TIMEOUT_MS)
+      break;
+    // safety timeout: if playback exceeds maximum expected duration, force exit
+    if (millis() - playbackBeganAt > Config::Audio::AUDIO_MAX_DURATION_MS) {
+      Serial.println("WARN: Playback timeout, forcing IDLE");
+      currentRunState = RunState::COOLDOWN;
+      playbackEndedAt = millis();
+      break;
+    }
     if (!isPlaying) {
       // only transition back to IDLE state when sound is done playing
-      currentProdRunState = ProductionRunState::IDLE;
+      currentRunState = RunState::COOLDOWN;
+
+      // create timestamp for when playback ended to begin cooldown period
+      playbackEndedAt = millis();
     } else {
-      // Serial.println("PLAYING");
+      Serial.println("PLAYING");
+    }
+    break;
+  case RunState::COOLDOWN:
+    // allow MPR121 to take readings/update delta during cooldown period but
+    // don't do anything with them, this is to ensure stale readings from last
+    // time and ema filter are updated
+    Serial.println("cooling down!");
+    currTouched = mpr121.touched();
+    lastTouched = currTouched;
+    if (millis() - playbackEndedAt >= Config::Audio::AUDIO_END_COOLDOWN_MS) {
+      currentRunState = RunState::IDLE;
     }
     break;
   }
-  lastTouched = currTouched;
 
   // put a delay so it isn't overwhelming
   delay(10);
