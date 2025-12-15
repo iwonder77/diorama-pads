4992cbd64a088191dd5aad5b122a788120a365e3
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-12-15 12:26:13 -0700

    feat(spotlight): implement Spotlight namespace free functions' logic

--- src/Spotlight.h
+++ src/Spotlight.h
@@ -1,10 +1,32 @@
+#pragma once
+
 #include <Arduino.h>
 
 #include "Config.h"
 
 namespace Spotlight {
-void init();
-void on(uint8_t index);
-void off(uint8_t index);
-void allOff();
+inline void init() {
+  for (uint8_t i = 0; i < Config::Spotlight::SPOTLIGHT_COUNT; i++) {
+    pinMode(Config::Spotlight::SPOTLIGHT_PINS[i], OUTPUT);
+    digitalWrite(Config::Spotlight::SPOTLIGHT_PINS[i], LOW);
+  }
+}
+inline void on(uint8_t index) {
+  if (index >= Config::Spotlight::SPOTLIGHT_COUNT) {
+    return;
+  }
+  digitalWrite(Config::Spotlight::SPOTLIGHT_PINS[index], HIGH);
+  return;
+}
+inline void off(uint8_t index) {
+  if (index >= Config::Spotlight::SPOTLIGHT_COUNT) {
+    return;
+  }
+  digitalWrite(Config::Spotlight::SPOTLIGHT_PINS[index], LOW);
+}
+inline void allOff() {
+  for (uint8_t i = 0; i < Config::Spotlight::SPOTLIGHT_COUNT; i++) {
+    digitalWrite(Config::Spotlight::SPOTLIGHT_PINS[i], LOW);
+  }
+}
 } // namespace Spotlight
